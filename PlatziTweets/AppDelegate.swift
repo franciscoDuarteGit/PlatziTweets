7b5400b248c25e7337dd3fe473a80a2610f12436
GioAtondo <gioatondo7@gmail.com>
2023-04-11 13:01:32 -0700

    Initial Commit

---
+++ PlatziTweets/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  PlatziTweets
+//
+//  Created by Usuario on 10/04/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow? // punto de entrada a la plicación
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    
+
+}
+
